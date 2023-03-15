77bf692553bcddc6061ca4b92fb6e0908ade3c15
hovien2201 <hovien30122017@gmail.com>
2023-03-15 17:13:59 +0700

    code_java

--- code_java/baitap/day7/bai_2/Test.java
+++ code_java/baitap/day7/bai_2/Test.java
@@ -5,8 +5,8 @@ public class Test {
     public static void main(String[] args) {
         Shape[] shape = new Shape[3];
         shape[0] = new Circle();
-        shape[1] = new Rectangle();
-        shape[2] = new Square();
+        shape[1] = new Square();
+        shape[2] = new Rectangle();
         System.out.println("Diện tích các hình:");
         for (Shape s : shape) {
             System.out.println(s);
